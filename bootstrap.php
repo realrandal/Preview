1f0de99d4f5702b1861bcf468e252142812d795e
Peter Brinck <peter@ac10.dk>
2014-12-24 12:25:27 +0100

    Added a dashboard widget
    I've added a dashboard widget, the only thing I need now is a free API
    that takes screenshot of the website

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,23 @@
 <?php
 
 if(COCKPIT_ADMIN) {
-    
-    $app->on("admin.init", function() use($app){
-               
-        // menu item
-        $app("admin")->menu("top", [
-            "url"    => $app->pathToUrl('site:'),
-            "label"  => '<i class="uk-icon-eye"></i>',
-            "title"  => $app("i18n")->get("View site")
-        ], -1);
-        
-    });
-    
+
+	$app->on("admin.init", function() use($app){
+	
+		// menu item
+		$app("admin")->menu("top", [
+				"url"    => $app->pathToUrl('site:'),
+				"label"  => '<i class="uk-icon-eye"></i>',
+				"title"  => $app("i18n")->get("View site")
+		], -2);
+		
+	});
+
+	$app->on('admin.dashboard.aside', function() {
+
+		$this->renderView('preview:views/dashboard.php with cockpit:views/layouts/dashboard.widget.php');
+	});
+
+
+
 }
