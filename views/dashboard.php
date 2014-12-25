d85f69cd5f0dd1b640d58aea67f06ef09248a149
Peter Brinck <AC10@users.noreply.github.com>
2014-12-25 14:45:50 +0100

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -26,7 +26,7 @@
 
 
 <div class="uk-margin-bottom">
-    <a href="<?php echo $url; ?>">
+    <a target="_blank" href="<?php echo $url; ?>">
         <img src="<?php echo $path; ?>" alt="Screenshot">
 <!-- <img id="p2i_demo" src="http://api.page2images.com/directlink?p2i_url=http://ac10.dk/projects/rhinobucket/&p2i_device=6&p2i_screen=1200x0&p2i_size=300x0&p2i_fullpage=1&p2i_refresh=1&p2i_key=662f87c1d144d63c" /> -->
     </a>
